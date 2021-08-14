8f1406c778951140d9a394d32fa12c8eff34095c
Doni-zete <donizete112@gmail.com>
2021-08-13 23:43:02 -0300

    adicionando rabo

--- script.js
+++ script.js
@@ -29,7 +29,7 @@ function criarCobrinha() {
 
 function drawFood() {
     context.fillStyle = "red";
-    context.fillRect(food.x,food.y,box,box);
+    context.fillRect(food.x, food.y, box, box);
 }
 
 
@@ -64,12 +64,23 @@ function iniciarjogo() {
     if (direction == "up") snakeY -= box;
     if (direction == "down") snakeY += box;
 
-    snake.pop();
-    let newHead = {
-        x: snakeX,
-        y: snakeY
+
+    if (snakeX != food.x || snakeY != food.y) {
+        snake.pop();
     }
-    snake.unshift(newHead);
+    else {food.x = Math.floor(Math.random() * 15 + 1) * box;
+    food.y = Math.floor(Math.random() * 15 + 1) * box;
+}
+
+
+
+
+
+let newHead = {
+    x: snakeX,
+    y: snakeY
+}
+snake.unshift(newHead);
 
 }
 
